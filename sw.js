78e9ad22d8b69c92dbabdeed42d9ccda6b54f150
Cigas337 <paulovieiraases71@gmail.com>
2026-08-24 15:29:09 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='pokedex-xl-v10-15-scanner';
+const CACHE_NAME='pokedex-xl-v10-16-scanner-fix';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
