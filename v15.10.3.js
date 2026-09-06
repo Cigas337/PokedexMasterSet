7ba863e5bf3ccac30730f761db8a11ba3bb37ec6
Cigas337 <paulovieiraases71@gmail.com>
2026-09-06 11:37:42 +0000

    chore: refresh cache for copy button layout

--- v15.10.3.js
+++ v15.10.3.js
@@ -127,7 +127,7 @@
 /* Carrega a função de cópia da identificação da carta (v15.10.4). */
 (function(){
   const script=document.createElement('script');
-  script.src='./v15.10.4.js?v=15.10.4';
+  script.src='./v15.10.4.js?v=15.10.5';
   script.async=true;
   document.head.appendChild(script);
 })();
